2e9f8aa01b71c0827cc4399681db1eccb7b09dc3
Miguel Penha <miguel.penha1@gmail.com>
2021-10-16 03:49:46 -0300

    Adicionando animações e build individual para cada app

--- App.js
+++ App.js
@@ -1,17 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
-import { Text, View, ScrollView, Image } from 'react-native'
+import { Text, View, ScrollView, Image, SafeAreaView } from 'react-native'
 import heroku from './apis/heroku'
 import { Svg, Path, Rect } from 'react-native-svg'
 import { StatusBar } from 'expo-status-bar'
-
-const Container = styled(View)`
-  flex: 1;
-  background-color: #ffffff;
-  align-items: center;
-  justify-content: center;
-  padding-bottom: 25%;
-`
+import Lottie from 'lottie-react-native'
 
 export default function App() {
   const [apps, setApps] = useState([{
@@ -36,27 +29,10 @@ export default function App() {
         }
       })
     })
-    let appsParaColocarCertas = Promise.all(
-      appsParaColocar.map(async app => {
-        const builds = await (await heroku.get(`/apps/${app.id}/builds`)).data
-        const pendingsBrutos = Promise.all(
-          builds.map(build => {
-            if (build.status === 'pending') {
-              return 'pending'
-            } else {
-              return 'sucess'
-            }
-          })
-        )
-        const pending = (await pendingsBrutos).includes('pending') ? true : false
-        app.build.pending = pending
-
-        return app
-      })
-    )
-      
-    return await appsParaColocarCertas
+    
+    return await appsParaColocar
   }
+
   useEffect(() => {
     appsGet().then(apps => {
       setApps(apps)
@@ -64,51 +40,56 @@ export default function App() {
     })
   }, [])
 
-  function CardApp({ children='', check=true }) {
+  const Container = styled(SafeAreaView)`
+    flex: 1;
+    background-color: #ffffff;
+    align-items: center;
+    justify-content: center;
+    padding-bottom: 25%;
+  `
+
+  const LoadingIcon = styled(Lottie)`
+    width: 120px;
+    height: 120px;
+    margin-top: 50%;
+  `
+
+  function CardApp({ children='', check=true, host='' }) {
+    const [build, setBuild] = useState()
+    const [loadingBuild, setLoadingBuild] = useState(false)
+    async function BuildsGet(app) {
+      const builds = await (await heroku.get(`/apps/${app}/builds`)).data
+      const pendingsBrutos = Promise.all(
+        builds.map(build => {
+          if (build.status === 'pending') {
+            return 'pending'
+          } else {
+            return 'sucess'
+          }
+        })
+      )
+      const pending = !(await pendingsBrutos).includes('pending') ? true : false
+      
+      setBuild(pending)
+      setLoadingBuild(true)
+    }
+
+    useEffect(() => {
+      BuildsGet(children)
+    }, [])
     const Card = styled(View)`
-      background-color: #0D2CD9;
+      background-color: #27BEE8;
       padding: 7%;
       border-radius: 20px;
       margin-top: 10%;
-      width: 85%;
+      width: 90%;
       display: flex;
-      flex-direction: row;
+      flex-direction: column;
       justify-content: flex-start;
     `
 
-    const Title = styled(Text)`
-      color: #ffffff;
-      font-size: 18px;
-      font-weight: bold;
-      width: 100%;
-      margin-left: 7%;
-    `
-
-    const IconBuild = styled(Svg)`
-      width: 100%;
-      padding: 0;
-      margin: 0;
-      right: 290%;
-      top: 10%;
-      padding-top: 30%;
-      padding-bottom: 10%;
-    `
-
-    const IconBuildText = styled(Text)`
-      right: 265%;
-      margin-top: 25%;
-      color: #ffffff;
-      font-size: 17px;
-    `
-
-    const IconCheck = styled(Svg)`
-      width: 100%;
-      padding: 0;
-      margin: 0;
-      right: 295%;
-      top: 10%;
-      padding-top: 20%;
-      padding-bottom: 20%;
+    const Header = styled(View)`
+      flex-direction: row;
     `
 
     const LogoHeroku = styled(Image)`
@@ -116,14 +97,81 @@ export default function App() {
       height: 30px;
     `
 
+    const Title = styled(Text)`
+      color: #ffffff;
+      font-size: 18px;
+      font-weight: bold;
+      width: 100%;
+      margin-left: 5%;
+    `
+
+    const Infos = styled(View)`
+      flex-direction: row;
+      align-items: center;
+      margin-top: 7%;
+      ${() => !loadingBuild &&'margin-bottom: 18%;'};
+    `
+
+    const IconCheck = styled(Svg)`
+      width: 40px;
+      height: 40px;
+    `
+
+    const IconBuild = styled(Svg)`
+      width: 40px;
+      height: 40px;
+    `
+
+    const BuildText = styled(Text)`
+      color: #ffffff;
+      background-color: #F2AE30;
+      font-size: 19px;
+      margin-left: 5%;
+      padding-left: 5%;
+      padding-right: 5%;
+      padding-top: 3.5%;
+      padding-bottom: 3.5%;
+      border-radius: 30px;
+    `
+
+    const SuccessText = styled(Text)`
+      color: #ffffff;
+      background-color: #29A678;
+      font-size: 19px;
+      margin-left: 5%;
+      padding-left: 5%;
+      padding-right: 5%;
+      padding-top: 3.5%;
+      padding-bottom: 3.5%;
+      border-radius: 30px;
+    `
+
+    const LoadingIconCard = styled(Lottie)`
+      width: 80px;
+      height: 80px;
+      padding-left: 20%;
+    `
+
     return (
       <Card>
-        <LogoHeroku source={require('./assets/heroku.png')}/>
-        <Title>{children}</Title>
-        {check ? <IconCheck xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="32px" viewBox="0 0 24 24" width="32px" fill="#29A678"><Rect fill="none" height="24" width="24"/><Path d="M22,5.18L10.59,16.6l-4.24-4.24l1.41-1.41l2.83,2.83l10-10L22,5.18z M19.79,10.22C19.92,10.79,20,11.39,20,12 c0,4.42-3.58,8-8,8s-8-3.58-8-8c0-4.42,3.58-8,8-8c1.58,0,3.04,0.46,4.28,1.25l1.44-1.44C16.1,2.67,14.13,2,12,2C6.48,2,2,6.48,2,12 c0,5.52,4.48,10,10,10s10-4.48,10-10c0-1.19-0.22-2.33-0.6-3.39L19.79,10.22z"/></IconCheck> : <>
-          <IconBuild xmlns="http://www.w3.org/2000/svg" height="32px" viewBox="0 0 24 24" width="32px" fill="#F2AE30"><Path d="M0 0h24v24H0V0z" fill="none"/><Path d="M22.61 18.99l-9.08-9.08c.93-2.34.45-5.1-1.44-7C9.79.61 6.21.4 3.66 2.26L7.5 6.11 6.08 7.52 2.25 3.69C.39 6.23.6 9.82 2.9 12.11c1.86 1.86 4.57 2.35 6.89 1.48l9.11 9.11c.39.39 1.02.39 1.41 0l2.3-2.3c.4-.38.4-1.01 0-1.41zm-3 1.6l-9.46-9.46c-.61.45-1.29.72-2 .82-1.36.2-2.79-.21-3.83-1.25C3.37 9.76 2.93 8.5 3 7.26l3.09 3.09 4.24-4.24-3.09-3.09c1.24-.07 2.49.37 3.44 1.31 1.08 1.08 1.49 2.57 1.24 3.96-.12.71-.42 1.37-.88 1.96l9.45 9.45-.88.89z"/></IconBuild>
-          <IconBuildText>Build...</IconBuildText>
-        </>}
+        <Header>
+          {host==='heroku' && <LogoHeroku source={require('./assets/heroku.png')}/>}
+          <Title>{children}</Title>
+        </Header>
+        <Infos>
+          {loadingBuild && (
+            <>
+              {build ? <>
+                <IconCheck xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" viewBox="0 0 24 24" fill="#29A678"><Rect fill="none" height="24" width="24"/><Path d="M22,5.18L10.59,16.6l-4.24-4.24l1.41-1.41l2.83,2.83l10-10L22,5.18z M19.79,10.22C19.92,10.79,20,11.39,20,12 c0,4.42-3.58,8-8,8s-8-3.58-8-8c0-4.42,3.58-8,8-8c1.58,0,3.04,0.46,4.28,1.25l1.44-1.44C16.1,2.67,14.13,2,12,2C6.48,2,2,6.48,2,12 c0,5.52,4.48,10,10,10s10-4.48,10-10c0-1.19-0.22-2.33-0.6-3.39L19.79,10.22z"/></IconCheck>
+                <SuccessText>Success</SuccessText>
+              </> : <>
+                <IconBuild xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#F2AE30"><Path clip-rule="evenodd" d="M0 0h24v24H0z" fill="none"/><Path d="M22.7 19l-9.1-9.1c.9-2.3.4-5-1.5-6.9-2-2-5-2.4-7.4-1.3L9 6 6 9 1.6 4.7C.4 7.1.9 10.1 2.9 12.1c1.9 1.9 4.6 2.4 6.9 1.5l9.1 9.1c.4.4 1 .4 1.4 0l2.3-2.3c.5-.4.5-1.1.1-1.4z"/></IconBuild>
+                <BuildText>Building</BuildText>
+              </>}
+            </>
+          )}
+          {!loadingBuild && <LoadingIconCard autoPlay={true} source={require('./animations/loadingWhite.json')}/>}
+        </Infos>
       </Card>
     )
   }
@@ -131,10 +179,9 @@ export default function App() {
   return (
     <ScrollView style={{flex: 5, backgroundColor: '#ffffff', padding: 20}}>
       <Container>
-        <Text>{appsLoading && apps[0].nome}</Text>
-        {appsLoading && apps.map((app, index) => <CardApp key={index} check={app.build.pending ? false : true}>{app.nome}</CardApp>)}
-        <CardApp key={10} check={false}>Teste Build</CardApp>
-        <StatusBar style="light" />
+        {appsLoading && apps.map((app, index) => <CardApp host="heroku" key={index} check={app.build.pending ? false : true}>{app.nome}</CardApp>)}
+        {!appsLoading && <LoadingIcon autoPlay={true} source={require('./animations/loading.json')}/>}
+        <StatusBar style="light"/>
       </Container>
     </ScrollView>
   )
